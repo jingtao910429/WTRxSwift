829522e73510f148e7ea7a222782c82cdc793430
魏武涛 <mac@jingtaoMac.local>
2017-09-14 15:37:48 +0800

    fix: RxSwift Button action

---
+++ lesson1/Lesson1/Lesson1/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Lesson1
+//
+//  Created by Mac on 2017/9/14.
+//  Copyright © 2017年 LiYou. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var showLabel: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    fileprivate var disposeBag: DisposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tapButton.rx.tap.subscribe({ [weak self] _ in
+            guard let `self` = self else {
+                return
+            }
+            guard let text = self.showLabel.text else {
+                return
+            }
+            guard let number = Int(text) else {
+                return
+            }
+            self.showLabel.text = String(number + 1)
+            
+        }).addDisposableTo(disposeBag)
+        
+        resetButton.rx.tap.subscribe({ [weak self] _ in
+            guard let `self` = self else {
+                return
+            }
+            self.showLabel.text = "0"
+        }).addDisposableTo(disposeBag)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
