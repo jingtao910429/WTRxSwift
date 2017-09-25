04758d266f57d7a30239e19538e8e31e6761de98
魏武涛 <mac@jingtaoMac.local>
2017-09-25 09:01:02 +0800

    fix: add RxTableView

--- lesson3/Lesson3/Lesson3/ViewController.swift
+++ lesson3/Lesson3/Lesson3/ViewController.swift
@@ -7,13 +7,48 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    fileprivate var disposeBag = DisposeBag()
+    fileprivate var refreshControl = UIRefreshControl()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        let items = Variable([
+            "Mike",
+            "Apples",
+            "Ham",
+            "Eggs"
+            ])
+        let itmes2 = [
+            "Fish",
+            "Carrots",
+            "Mike",
+            "Apples",
+            "Ham",
+            "Eggs",
+            "Bread",
+            "Chiken",
+            "Water"
+        ]
+        
+        items.asObservable()
+            .bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self), curriedArgument: { (row, element, cell) in
+                cell.textLabel?.text = element
+            }).disposed(by: disposeBag)
+        refreshControl.rx
+            .controlEvent(.valueChanged)
+            .subscribe(onNext: { _ in
+                items.value = itmes2
+                self.refreshControl.endRefreshing()
+            }).addDisposableTo(disposeBag)
+        tableView.addSubview(refreshControl)
     }
 
     override func didReceiveMemoryWarning() {
